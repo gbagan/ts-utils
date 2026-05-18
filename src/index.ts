59bc92cb248abefb2a8b7c2e8e178b05c82f6fdd
Guillaume Bagan <gbagan@gmail.com>
2026-05-18 07:51:38 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,111 @@
+// Returns an array of length n where every element is val.
+export const arrayOf = <A>(n: number, val: A): A[] => new Array(n).fill(val);
+
+// Returns an array of length n where the i-th element is f(i).
+export const times = <A>(n: number, f: (i: number) => A): A[] => Array.from({ length: n }, (_, i) => f(i));
+
+// Returns an array of numbers from start (inclusive) to end (exclusive) with a step of step.
+// If step is not provided, it defaults to 1.
+export function range(start: number, end: number, step?: number): number[] {
+  const res = [];
+  step = step ?? 1;
+  if (step > 0) {
+    for (let i = start; i < end; i += step) {
+      res.push(i);
+    }
+  } else {
+    for (let i = start; i > end; i += step) {
+      res.push(i);
+    }
+  }
+  return res
+}
+
+// Returns an array of the results of applying f to each element of the input array,
+// excluding any null results.
+export function filterMap<A, B>(arr: readonly A[], f: (x: A, i: number) => B | null): B[] {
+  const res: B[] = [];
+  const n = arr.length;
+  for (let i = 0; i < n; i++) {
+    const x = f(arr[i]!, i);
+    if (x !== null) {
+      res.push(x);
+    }
+  }
+  return res;
+}
+
+
+// Counts the number of elements in the array that satisfy the given predicate.
+export function count<A>(arr: readonly A[], pred: (x: A, i: number) => boolean): number {
+  let count = 0;
+  const n = arr.length;
+  for (let i = 0; i < n; i++) {
+    if (pred(arr[i]!, i)) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+// Returns the element of the array that minimizes the given function. Returns null if the array is empty.
+export function minBy<A>(arr: readonly A[], fn: (x: A, i: number) => number) {
+  let min = null;
+  let bestScore = Infinity;
+  const n = arr.length;
+  for (let i = 0; i < n; i++) {
+    const x = arr[i]!;
+    const score = fn(x, i);
+    if (score < bestScore) {
+      bestScore = score;
+      min = x;
+    } 
+  }
+  return min;
+}
+
+// Returns the element of the array that maximizes the given function. Returns null if the array is empty.
+export const maxBy = <A>(arr: readonly A[], fn: (x: A, i: number) => number) => minBy(arr, (x, i) => -fn(x, i))
+
+// Returns an array of the elements of the input array that maximize the given function.
+// Returns an empty array if the input array is empty.
+export const maximaBy = <A>(arr: readonly A[], f: (a: A, i: number) => number): A[] => {
+  let maxVal = -Infinity;
+  const res: A[] = [];
+  const n = arr.length;
+  for (let i = 0; i < n; i++) {
+    const a = arr[i]!;
+    const val = f(a, i);
+    if (val > maxVal) {
+      maxVal = val;
+      res.length = 0;
+      res.push(a);
+    } else if (val === maxVal) {
+      res.push(a);
+    }
+  }
+  return res;
+}
+
+// Partitions the array into two arrays: the first contains the elements that satisfy the predicate,
+// and the second contains the elements that do not.
+export function partition<A>(xs: A[], f: (x: A) => boolean): [A[], A[]] {
+  const yes: A[] = [];
+  const no: A[] = [];
+  const n = xs.length;
+  for (let i = 0; i < n; i++) {
+    const x = xs[i]!;
+    if (f(x)) { yes.push(x) } else  { no.push(x) };
+  }
+  return [yes, no];
+}
+
+
+
+// Returns the quotient and remainder of n divided by m.
+export const divMod = (n: number, m: number): [number, number] => [Math.floor(n / m), n % m];
+
+
+
+// Returns a promise that resolves after ms milliseconds.
+export const sleep = <A>(ms: number) => new Promise<A>(resolve => setTimeout(resolve, ms));
